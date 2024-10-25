893ab2699a888d0ea4d4d72765a968d2e15357ea
thierry-msm <thierrymarcoff@gmail.com>
2024-10-25 17:02:32 -0300

    otimização dispositivos móveis

--- carrinho.js
+++ carrinho.js
@@ -10,21 +10,26 @@ function carregarCarrinho() {
 
     carrinho.forEach((produto, index) => {
         const produtoElemento = document.createElement('div');
-        produtoElemento.classList.add('product-cart'); // Mantenha a classe correta para o estilo do carrinho
+        produtoElemento.classList.add('product-cart'); // Classe para estilizar os produtos do carrinho
         produtoElemento.innerHTML = `
             <img src="${produto.thumbnail}" alt="${produto.title}">
             <h3>${produto.title}</h3>
-            <p>Marca: ${produto.brand}</p>
-            <p>R$${produto.price.toFixed(2)}</p>
-            <p>Quantidade: <input type="number" value="${produto.quantidade}" min="1" onchange="alterarQuantidade(${index}, this.value)"></p>
-            <button onclick="removerDoCarrinho(${index})">Remover</button>
+            <div class="product-info">
+                <p>Marca: ${produto.brand} <span class="product-price">R$${produto.price.toFixed(2)}</span></p>
+                <div class="product-quantity">
+                    <button class="quantity-btn" onclick="alterarQuantidade(${index}, ${produto.quantidade} - 1)">-</button>
+                    <input type="number" value="${produto.quantidade}" min="1" onchange="alterarQuantidade(${index}, this.value)">
+                    <button class="quantity-btn" onclick="alterarQuantidade(${index}, ${produto.quantidade} + 1)">+</button>
+                </div>
+            </div>
+            <button class="remove-item" onclick="removerDoCarrinho(${index})">Remover</button>
         `;
         listaCarrinho.appendChild(produtoElemento);
         total += produto.price * produto.quantidade; // Acumula o preço total de todos os produtos no carrinho
     });
-
+    
     totalValueElem.textContent = total.toFixed(2); // Atualiza o valor total do carrinho
-}
+}    
 
 // Função para remover produto do carrinho
 function removerDoCarrinho(index) {
@@ -38,6 +43,13 @@ function removerDoCarrinho(index) {
 // Função para alterar quantidade
 function alterarQuantidade(index, quantidade) {
     carrinho[index].quantidade = parseInt(quantidade);
+
+     // Garante que a quantidade nunca seja menor que 1
+     quantidade = Math.max(1, parseInt(quantidade));
+
+     // Atualiza a quantidade no carrinho
+     carrinho[index].quantidade = quantidade;
+
     localStorage.setItem('carrinho', JSON.stringify(carrinho));
     carregarCarrinho(); // Atualiza o valor total do carrinho após a alteração
 }
